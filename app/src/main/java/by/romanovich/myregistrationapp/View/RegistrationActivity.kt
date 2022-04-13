864fd03694f181e2f39d5ed2e5f5154401541860
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-04-13 15:01:30 +0300

    Сделал Добавил заглушки, что надо сохраняется.

--- app/src/main/java/by/romanovich/myregistrationapp/View/RegistrationActivity.kt
+++ app/src/main/java/by/romanovich/myregistrationapp/View/RegistrationActivity.kt
@@ -1,12 +1,11 @@
-package by.romanovich.myregistrationapp
+package by.romanovich.myregistrationapp.View
 
-import android.app.Activity
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import by.romanovich.myregistrationapp.R
 import by.romanovich.myregistrationapp.databinding.ActivityRegistrationBinding
 import by.romanovich.myregistrationapp.presenter.LoginContract
 import by.romanovich.myregistrationapp.presenter.LoginPresenter
@@ -28,6 +27,15 @@ class RegistrationActivity : AppCompatActivity(), LoginContract.RepositoryView {
                 binding.editPassword.text.toString()
             )
         }
+
+        binding.lostLoginOrPassword.setOnClickListener {
+            Toast.makeText(this, R.string.lost_your_login_or_password, Toast.LENGTH_SHORT).show()
+        }
+        binding.createAcc.setOnClickListener {
+            Toast.makeText(this, R.string.create_account, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
     override fun setRegister() {
@@ -51,15 +59,17 @@ class RegistrationActivity : AppCompatActivity(), LoginContract.RepositoryView {
     }
 
     override fun hideProgress() {
-        binding.cardConteiner.visibility = View.GONE
+        binding.cardConteiner.visibility = View.VISIBLE
     }
 
     //метод что бы достать презентор(объект) сохраненный в lastCustomNonConfigurationInstance если он LoginPresenter то мы его сохраняем
     private fun restorePresenter(): LoginContract.RepositoryPresenter {
         val presenter = lastCustomNonConfigurationInstance as? LoginPresenter
+        //возращаем presenter, но если он ноль то создаем новый презентер
         return presenter ?: LoginPresenter()
     }
 
+    //метод что бы положить объект
     override fun onRetainCustomNonConfigurationInstance(): Any {
         return presenter
     }
