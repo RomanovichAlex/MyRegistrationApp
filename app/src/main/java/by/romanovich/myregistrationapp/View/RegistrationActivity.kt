75581d2246c5b0793bb57e2c7d167650ab7073f1
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-04-20 13:29:13 +0300

    Переделал codeStyle, в макете использую snake_case.

--- app/src/main/java/by/romanovich/myregistrationapp/View/RegistrationActivity.kt
+++ app/src/main/java/by/romanovich/myregistrationapp/View/RegistrationActivity.kt
@@ -21,43 +21,43 @@ class RegistrationActivity : AppCompatActivity(), LoginContract.RepositoryView {
         presenter = restorePresenter()
         presenter.onAttach(this)
 
-        binding.buttonLogin.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             presenter.onLogin(
-                binding.editLogin.text.toString(),
-                binding.editPassword.text.toString()
+                binding.loginEditText.text.toString(),
+                binding.passwordEditText.text.toString()
             )
         }
 
         binding.lostLoginOrPassword.setOnClickListener {
             Toast.makeText(this, R.string.lost_your_login_or_password, Toast.LENGTH_SHORT).show()
         }
-        binding.createAcc.setOnClickListener {
+        binding.createAccTextView.setOnClickListener {
             Toast.makeText(this, R.string.create_account, Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun setRegister() {
-        Toast.makeText(this, getString(R.string.EnterTheData), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.enter_the_data), Toast.LENGTH_SHORT).show()
     }
 
     override fun setSuccess() {
-        binding.cardConteiner.setBackgroundColor(Color.GREEN)
-        Toast.makeText(this, getString(R.string.Succes), Toast.LENGTH_SHORT).show()
+        binding.cardViewConteiner.setBackgroundColor(Color.GREEN)
+        Toast.makeText(this, getString(R.string.succes), Toast.LENGTH_SHORT).show()
         hideProgress()
     }
 
     override fun setError(error: String){
         showProgress()
-        binding.cardConteiner.setBackgroundColor(Color.RED)
+        binding.cardViewConteiner.setBackgroundColor(Color.RED)
         Toast.makeText(this, " " + "$error", Toast.LENGTH_SHORT).show()
     }
 
     override fun showProgress() {
-        binding.cardConteiner.visibility = View.VISIBLE
+        binding.cardViewConteiner.visibility = View.VISIBLE
     }
 
     override fun hideProgress() {
-        binding.cardConteiner.visibility = View.VISIBLE
+        binding.cardViewConteiner.visibility = View.VISIBLE
     }
 
     //метод что бы достать презентор(объект) сохраненный в lastCustomNonConfigurationInstance если он LoginPresenter то мы его сохраняем
